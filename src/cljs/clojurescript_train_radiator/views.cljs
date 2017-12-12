8bdd3b22291d2913effc4dada8852e6d024d63b9
Markku Rontu <markku.rontu@iki.fi>
2017-12-12 11:30:36 +0200

    feat: use material-ui table for train info

--- src/cljs/clojurescript_train_radiator/views.cljs
+++ src/cljs/clojurescript_train_radiator/views.cljs
@@ -14,7 +14,7 @@
 ;;
 
 (defn show-time [t]
-  (localtime/format-local-time t :basic-date-time #_:hour-minute))
+  (localtime/format-local-time t :hour-minute))
 
 (defn in-the-future? [t]
   (time/before? (localtime/local-now) (localtime/to-local-date-time t)))
@@ -26,26 +26,28 @@
 (defn timetable [row]
   (let [scheduled (:scheduledTime row)
         actual (:actualTime row)]
-    [:li
-     (show-time scheduled)
-     (when actual
-       (list
-        "->"
-        (show-time actual)))
-     (when (in-the-future? scheduled)
-       "***")
-     " "
-     (:type row)
-     " "
-     (:stationShortCode row)]))
+    [ui/table-row
+     [ui/table-row-column (show-time scheduled)]
+     [ui/table-row-column
+      (when actual (show-time actual))]
+     [ui/table-row-column (in-the-future? scheduled)]
+     [ui/table-row-column (:type row)]
+     [ui/table-row-column (:stationShortCode row)]]))
 
 (defn train [t]
   [ui/card
-   [ui/card-title (:trainNumber t) " " (:trainType t) " " (:trainCategory t) " " (:commuterLineID t)]
+   [ui/card-title "Train" (:trainNumber t) " " (:trainType t) " " (:trainCategory t) " " (:commuterLineID t)]
    [ui/card-text
-    [:ul
-     (map-indexed (fn [i row] ^{:key i} [timetable row])
-                  (take 5 (:timeTableRows t)))]]])
+    [ui/table
+     [ui/table-header
+      [ui/table-header-column "Scheduled"]
+      [ui/table-header-column "Actual"]
+      [ui/table-header-column "Future?"]
+      [ui/table-header-column "Type"]
+      [ui/table-header-column "Station"]]
+     [ui/table-body
+      (map-indexed (fn [i row] ^{:key i} [timetable row])
+                   (take 5 (:timeTableRows t)))]]]])
 
 (defn render [trains]
   [:div.trains
@@ -63,14 +65,15 @@
      (if @loading-stations?
        [:p "Loading, please wait..."]
        [:div
-        [ui/auto-complete {:hint-text "Choose station"
-                           :search-text (:stationComputedName @station)
-                           :on-focus #(re-frame/dispatch [:set-station ""])
-                           :on-new-request #(re-frame/dispatch [:set-station (js->clj % :keywordize-keys true)])
-                           :open-on-focus true
-                           :filter (fn [text key] (s/includes? (s/lower-case key) (s/lower-case text)))
-                           :dataSource @stations
-                           :dataSourceConfig {:text "stationComputedName" :value "stationShortCode"} }]
+        [:div.controls
+         [ui/auto-complete {:hint-text "Choose station"
+                            :search-text (:stationComputedName @station)
+                            :on-focus #(re-frame/dispatch [:set-station ""])
+                            :on-new-request #(re-frame/dispatch [:set-station (js->clj % :keywordize-keys true)])
+                            :open-on-focus true
+                            :filter (fn [text key] (s/includes? (s/lower-case key) (s/lower-case text)))
+                            :dataSource @stations
+                            :dataSourceConfig {:text "stationComputedName" :value "stationShortCode"} }]]
         (if @loading-trains?
           [:p "Loading, please wait..."]
           [render @trains])])]))
